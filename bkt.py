d6a3a797921d477e4383a310c8a9f6d37c2f2043
Laura Moraes <laura.moraes@twistsystems.com>
2019-09-08 13:51:31 -0300

    BKT class done + unit tests + log config file

--- bkt.py
+++ bkt.py
@@ -4,6 +4,15 @@ import subprocess
 import os
 import uuid
 import shlex
+import numpy as np
+import re
+import logging
+import logging.config
+
+logging.config.fileConfig(fname='file.conf', disable_existing_loggers=False)
+
+# Get the logger specified in the file
+LOGGER = logging.getLogger(__name__)
 
 algorithm_lookup = {
     "bw": "1.1",
@@ -26,6 +35,21 @@ class BKT(object):
         # Params
         self.params = None
 
+        # Separate skill params from general model params
+        self.model_params = ["SolverId", "nK", "nG", "nS", "nO", "nZ",
+                             "Null skill ratios"]
+
+        # Evaluation metrics
+        self.n_skills = 0
+        self.n_questions = 0
+        self.outcomes = None
+        self.loglikelihood = 0
+        self.outcome_prob = 0
+        self.aic = 0
+        self.bic = 0
+        self.rmse = 0
+        self.acc = 0
+
     def _create_data_file(self, data, q_matrix):
         if not os.path.exists("hmm_files"):
             os.makedirs("hmm_files")
@@ -42,6 +66,36 @@ class BKT(object):
                 step_file.write("%s\t%s\t%s\t%s\n" % (outcome, student_id, question_id, skills))
         return filename
 
+    # Probability of being in the learning state given that the student got the
+    # question correctly
+    def _correct(self, learning_state, skills_idx):
+        learning_correct = learning_state * (1 - self.S[skills_idx])
+        guess_correct = ((1 - learning_state) * self.G[skills_idx])
+        learning_evidence = learning_correct/(learning_correct + guess_correct)
+        return learning_evidence
+
+    # Probability of being in the learning state given that the student got the
+    # question wrongly
+    def _incorrect(self, learning_state, skills_idx):
+        learning_incorrect = learning_state * self.S[skills_idx]
+        guess_incorrect = ((1 - learning_state) * (1 - self.G[skills_idx]))
+        learning_evidence = learning_incorrect/(learning_incorrect + guess_incorrect)
+        return learning_evidence
+
+    # Update learning state probability
+    def _update(self, learning_state, skills_idx, iscorrect=True):
+        if iscorrect:
+            learning_evidence = self._correct(learning_state, skills_idx)
+        else:
+            learning_evidence = self._incorrect(learning_state, skills_idx)
+        learning_state = learning_evidence + ((1 - learning_evidence) * self.T[skills_idx])
+        return learning_state
+
+    # Predict whether the next question will be answered correctly or
+    # incorrectly by the student
+    def _get_correct_prob(self, learning_state, skills_idx):
+        return (1-self.S[skills_idx])*learning_state + self.G[skills_idx]*(1-learning_state)
+
     def download(self):
         """  This implementation is a wrapper around the
         HMM-scalable tool ( http://yudelson.info/hmm-scalable).
@@ -53,8 +107,8 @@ class BKT(object):
 
         Notes
         -----
-        This is a wrapper around the HMM-scalable tool (http://yudelson.info/hmm-scalable).
-        """
+        This is a wrapper around the HMM-scalable tool
+        (http://yudelson.info/hmm-scalable).  """
 
         # Download zipfile from GitHub
 #         results = requests.get('https://github.com/myudelson/hmm-scalable/archive/master.zip')
@@ -135,9 +189,8 @@ class BKT(object):
         idx = 0
         while idx < len(params_regex):
             param, value = params_regex[idx]
-            # Get skills. They start at zero.
-            try:
-                skill = int(param)
+            # If param is not listed in model params, it's a skill param
+            if param not in self.model_params:
                 params["skills"].append({
                     "skill": value,
                     # Get PI matrix
@@ -147,115 +200,143 @@ class BKT(object):
                     # Get B matrix
                     "emissions": np.asarray([float(i) for i in params_regex[idx+3][1].split("\t")])
                 })
+                LOGGER.debug("Appending skill %s params: %s" % (value, params["skills"]))
                 idx += 4
-            except ValueError:
+            else:
                 params[param] = value
                 idx += 1
 
         self.params = params
         return self
 
-    def _predict(self, data, q_matrix, model_file=None):
+    def _predict(self, data, q_matrix, learning_state=None):
         """ Predict student outcomes based on trained model.
 
         Parameters
         ----------
-        data : {array-like}, shape (n_steps, 3)
-            Sequence of students steps. Each of the three dimensions are:
+        data : {array-like}, shape (n_steps, 2)
+            Sequence of student steps. Each of the three dimensions are:
             Observed outcome: 0 for fail and 1 for success
-            Student id: student unique identifier
             Question id: question id in q_matrix
 
-        Returns
-        -------
-        outcome : array, shape (n_steps,)
-            Predicted outcomes for steps in data
+        q_matrix: matrix, shape (n_questions, n_concepts)
+            Each row is a question and each column a concept.
+            If the concept is present in the question, the
+            correspondent cell should contain 1, otherwise, 0.
 
-        Notes
-        -----
-        This is a wrapper around the HMM-scalable tool (http://yudelson.info/hmm-scalable)
-        """
-        PARAMS_ORDER = ["SolverId", "nK", "nG", "nS", "nO", "nZ", "Null skill ratios"]
-
-        # Create data file
-        filename = self._create_data_file(data, q_matrix)
-
-        # If model file does not exists, create one.
-        if not model_file:
-            model_file = "%s_model.txt" % filename
-
-            # Create model file from set params
-            with open(model_file, "w") as param_file:
-                model_params = self.get_params()
-
-                # Write general parameters to file
-                for param in PARAMS_ORDER:
-                    param_file.write("%s\t%s\n" % (param, model_params[param]))
-
-                # Write skill parameters to file
-                for idx, skill in enumerate(model_params["skills"]):
-                    priors = "\t".join(["%.10f" % element for element in skill["priors"]])
-                    transitions = "\t".join(["%.10f" % element for element in skill["transitions"]])
-                    emissions = "\t".join(["%.10f" % element for element in skill["emissions"]])
-                    skill_text = "%d\t%s\nPI\t%s\nA\t%s\nB\t%s\n" % (
-                        idx, skill["skill"], priors, transitions, emissions)
-                    param_file.write(skill_text)
-
-        # Run predict program
-        command = "./predicthmm -p 1 -d ~ ../%s.txt ../%s_model.txt ../%s_predict.txt" % (
-            filename, filename, filename)
-        args = shlex.split(command)
-        process = subprocess.Popen(args, stdout=subprocess.PIPE, stderr=subprocess.PIPE, cwd=self.hmm_folder)
-        process.wait()
-        stdout, stderr = process.communicate()
-        if process.returncode != 0:
-            raise RuntimeError("Could not predict HMM model. Check if the HMM files are properly created and "
-                               "accessible.\n"
-                               "Code: %d\n"
-                               "Error: %s" % (process.returncode, stderr))
-
-        # Read from predict file
-        with open("%s_predict.txt" % filename, "r") as predict_file:
-            y_pred_txt = predict_file.read().strip().split("\n")
-            y_pred_proba = []
-            for row in y_pred_txt:
-                y_pred_proba.append([float(i) for i in row.split("\t")])
-
-        # Swap columns so column 0 is outcome 0 (incorrect) and column 1 is outcome 1 (correct)
-        y_pred_proba = np.asarray(y_pred_proba)
-        y_pred_proba_tmp = y_pred_proba.copy()
-        y_pred_proba[:,0] = y_pred_proba_tmp[:,1]
-        y_pred_proba[:,1] = y_pred_proba_tmp[:,0]
-
-        return y_pred_proba
-
-    def predict_proba(self, data, q_matrix, model_file=None):
-        """ Predict student outcome probabilities based on trained model.
-
-        Parameters
-        ----------
-        data : {array-like}, shape (n_steps, 3)
-            Sequence of students steps. Each of the three dimensions are:
-            Observed outcome: 0 for fail and 1 for success
-            Student id: student unique identifier
-            Question id: question id in q_matrix
+        learning_state: {array-like}, shape (n_skills,)
+            Set learning state for each skill. The skills should be in the same
+            order as in the model parameters.
 
         Returns
         -------
         outcome : {array-like}, shape (n_steps, 2)
-            Outcome probabilites for steps in data. Column 0 corresponds to outcome 0 (incorrect)
-            and column 1 to outcome 1 (correct)
+            Outcome probabilites for steps in data. Column 0 corresponds to
+            outcome 0 (incorrect) and column 1 to outcome 1 (correct)
 
         Notes
         -----
-        This is a wrapper around the HMM-scalable tool (http://yudelson.info/hmm-scalable)
+        This calculates the predicted steps in the same way it is done in the
+        HMM-scalable tool (http://yudelson.info/hmm-scalable)
+
         """
-        y_pred_proba = self._predict(data, q_matrix, model_file)
+
+        # Get model params
+        model_params = self.get_params()
+        self.n_skills = len(model_params['skills'])
+
+        # Construct params matrices
+        skills = np.zeros(self.n_skills)
+
+        # If learning state is not set, use prior probabilities
+        if not learning_state:
+            learning_state = np.zeros(self.n_skills)
+
+        self.T = np.zeros(self.n_skills)
+        self.S = np.zeros(self.n_skills)
+        self.G = np.zeros(self.n_skills)
+        for idx, skill in enumerate(model_params['skills']):
+            skills[idx] = skill['skill']
+
+            # Update learning using priors if it was not set
+            if learning_state is None:
+                learning_state[idx] = skill['priors'][0]
+
+            # Update params
+            self.T[idx] = skill['transitions'][2]
+            self.S[idx] = skill['emissions'][1]
+            self.G[idx] = skill['emissions'][2]
+
+        data = np.asarray(data)
+        self.outcomes = data[:, 0]
+        self.n_questions = len(self.outcomes)
+        self.loglikelihood = np.zeros(self.n_questions)
+        outcome_prob_skill = np.zeros((self.n_skills, self.n_questions))
+        self.outcome_prob = np.zeros((self.n_questions, 2))
+
+        for idx, outcome in enumerate(self.outcomes):
+            # Get question skills from q_matrix
+            skills_idx = np.where(q_matrix[data[idx, 1], :] == 1)
+
+            # Sliced learning states (skill is present in this question)
+            l_sliced = learning_state[skills_idx]
+
+            # Calculate chance of being correct for each skill
+            outcome_prob_skill[skills_idx, idx] = self._get_correct_prob(l_sliced, skills_idx)
+            # Column 0 is probability of failing and column 1 is probability of
+            # success
+            self.outcome_prob[idx, 1] = outcome_prob_skill[:, idx].sum()/skills_idx[0].shape[0]
+            self.outcome_prob[idx, 0] = 1-self.outcome_prob[idx, 1]
+
+            # Calculate LL and update state
+            if outcome == 1:
+                ll_local = np.log(self.outcome_prob[idx, 1])
+                l_sliced = self._update(l_sliced, skills_idx, True)
+            else:
+                ll_local = np.log(self.outcome_prob[idx, 0])
+                l_sliced = self._update(l_sliced, skills_idx, False)
+            learning_state[skills_idx] = l_sliced
+
+            self.loglikelihood[idx] += ll_local
+
+        return self.outcome_prob
+
+    def predict_proba(self, data, q_matrix, learning_state=None):
+        """ Predict student outcome probabilities based on trained model.
+
+        Parameters
+        ----------
+        data : {array-like}, shape (n_steps, 2)
+            Sequence of student steps. Each of the three dimensions are:
+            Observed outcome: 0 for fail and 1 for success
+            Question id: question id in q_matrix
+
+        q_matrix: matrix, shape (n_questions, n_concepts)
+            Each row is a question and each column a concept.
+            If the concept is present in the question, the
+            correspondent cell should contain 1, otherwise, 0.
+
+        learning_state: {array-like}, shape (n_skills,)
+            Set learning state for each skill. The skills should be in the same
+            order as in the model parameters.
+
+        Returns
+        -------
+        outcome : {array-like}, shape (n_steps, 2)
+            Outcome probabilites for steps in data. Column 0 corresponds to
+            outcome 0 (incorrect) and column 1 to outcome 1 (correct)
+
+        Notes
+        -----
+        This calculates the predicted steps in the same way it is done in the
+        HMM-scalable tool (http://yudelson.info/hmm-scalable)
+        """
+        y_pred_proba = self._predict(data, q_matrix, learning_state)
         return y_pred_proba
 
     def predict(self, data, q_matrix, model_file=None):
-        """ Predict student outcomes based on trained model. This is just the hard-assigment
-        (highest probability) of the outcome probabilities.
+        """ Predict student outcomes based on trained model. This is just the
+        hard-assigment (highest probability) of the outcome probabilities.
 
         Parameters
         ----------
@@ -268,15 +349,39 @@ class BKT(object):
         Returns
         -------
         outcome : {array-like}, shape (n_steps,)
-            Outcomes for steps in data. Outcome 0 is incorrect and outcome 1 is correct.
+            Outcomes for steps in data. Outcome 0 is incorrect and outcome 1 is
+            correct.
 
         Notes
         -----
-        This is a wrapper around the HMM-scalable tool (http://yudelson.info/hmm-scalable)
+        This calculates the predicted steps in the same way it is done in the
+        HMM-scalable tool (http://yudelson.info/hmm-scalable)
         """
         y_pred_proba = self._predict(data, q_matrix, model_file)
         y_pred = np.argmax(y_pred_proba, axis=1)
         return y_pred
+
+    def score(self):
+        """
+        Calculates AIC, BIC, RMSE and accuracy for the predicted sample
+
+        """
+        # AIC
+        self.aic = -2*self.loglikelihood.sum() + 2*4*self.n_skills
+
+        # BIC
+        self.bic = -2*self.loglikelihood.sum() + 4*self.n_skills*np.log(
+            self.n_questions)
+
+        # RMSE
+        rmse = ((1-self.outcome_prob[range(self.n_questions), self.outcomes])**2).sum()
+        self.rmse = np.sqrt(rmse/self.n_questions)
+
+        # Accuracy
+        estimated_outcome = np.argmax(self.outcome_prob, axis=1)
+        self.acc = (estimated_outcome == self.outcomes).sum()/self.n_questions
+
+        return self.aic, self.bic, self.rmse, self.acc
 
     def get_params(self):
         """ Get fitted params.
---
+++ test.py
@@ -0,0 +1,220 @@
+import unittest
+import os
+import numpy as np
+from bkt import BKT
+import logging
+
+LOGGER = logging.getLogger(__name__)
+
+
+class TestBKT(unittest.TestCase):
+    """ Unit test class to test BKT module """
+    def setUp(self):
+        self.PARAMS_KEYS = {
+            'skills': list, 'SolverId': str, 'nK': str, 'nG': str,
+            'nS': str, 'nO': str, 'nZ': str, 'Null skill ratios': str
+        }
+        self.SKILLS_KEYS = {
+            "skill": str, "priors": np.ndarray,
+            "transitions": np.ndarray, "emissions": np.ndarray
+        }
+
+    def test_download(self):
+        """ Testing HMM-scalable download """
+        model = BKT()
+        model.download()
+
+        # Check if directory exists and it contains items
+        self.assertGreater(len(os.listdir(model.hmm_folder)), 1)
+
+    def test_fit(self):
+        """ Testing if fit tool is able to run and fit data """
+
+        # Data matrix
+        outcomes = [0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0]
+        data = []
+        for question_id, outcome in enumerate(outcomes):
+            data.append([outcome, 0, question_id])
+
+        # Q matrix
+        q_matrix = np.array([[1, 0]]*len(outcomes))
+        q_matrix[[0, 1], 1] = 1
+
+        # Instantiate model
+        model = BKT()
+        model.fit(data, q_matrix)
+
+        # Make sure self.params variable is complete after this test
+        self.assertIsNotNone(model.params)
+
+    def test_params_format(self):
+        """ Testing if fitted params are on the expected format """
+        # Data matrix
+        outcomes = [0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0]
+        data = []
+        for question_id, outcome in enumerate(outcomes):
+            data.append([outcome, 0, question_id])
+
+        # Q matrix
+        q_matrix = np.array([[1, 0]]*len(outcomes))
+        q_matrix[[0, 1], 1] = 1
+
+        # Instantiate model
+        model = BKT()
+        model.fit(data, q_matrix)
+
+        params = model.get_params()
+
+        for key, value in self.PARAMS_KEYS.items():
+            # Assert that all keys are there
+            self.assertIn(key, params)
+
+            # Assert key type is correct
+            self.assertTrue(isinstance(params[key], value))
+
+        for skill_param in params["skills"]:
+            for key, value in self.SKILLS_KEYS.items():
+                # Assert that all keys are there
+                self.assertIn(key, skill_param)
+
+                # Assert key type is correct
+                self.assertTrue(isinstance(skill_param[key], value))
+
+    # def test_params_values(self):
+    #     """ Testing if fitted params returned the expected values """
+    #     # Data matrix
+    #     outcomes = [0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0]
+    #     data = []
+    #     for question_id, outcome in enumerate(outcomes):
+    #         data.append([outcome, 0, question_id])
+
+    #     # Q matrix
+    #     q_matrix = np.array([[1, 0]]*len(outcomes))
+    #     q_matrix[[0, 1], 1] = 1
+
+    #     # Instantiate model
+    #     model = BKT()
+    #     model.fit(data, q_matrix)
+
+    #     params = model.get_params()
+    #     expected_params = {
+    #         'skills': [
+    #             {'skill': '0',
+    #              'priors': np.array([0.0000000000, 1.0000000000]),
+    #              'transitions': np.array([1.0000000000, 0.0000000000,
+    #                                       0.1667616137, 0.8332383863]),
+    #              'emissions': np.array([0.9995594072, 0.0004405928,
+    #                                     0.0003857303, 0.9996142697])},
+    #             {'skill': '1',
+    #              'priors': np.array([0.0000068251, 0.9999931749]),
+    #              'transitions': np.array([1.0000000000, 0.0000000000,
+    #                                       0.0013481800, 0.9986518200]),
+    #              'emissions': np.array([0.7000000000, 0.3000000000,
+    #                                     0.0000000000, 1.0000000000])}],
+    #         'SolverId': '1.1', 'nK': '2', 'nG': '1', 'nS': '2', 'nO': '2',
+    #         'nZ': '1', 'Null skill ratios': '  1.0000000\t  0.0000000'}
+
+    #     self.assertEqual(params, expected_params)
+
+    def test_predict_proba(self):
+        """ Testing probability predict function """
+        # Data matrix
+        outcomes = [0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0]
+        data = []
+        for question_id, outcome in enumerate(outcomes):
+            data.append([outcome, 0, question_id])
+
+        # Q matrix
+        q_matrix = np.array([[1, 0]]*len(outcomes))
+        q_matrix[[0, 1], 1] = 1
+
+        # Instantiate model
+        model = BKT()
+        model.fit(data, q_matrix)
+
+        # Data for predicting should contain only one student
+        data = []
+        for question_id, outcome in enumerate(outcomes):
+            data.append([outcome, question_id])
+        y_pred_proba = model.predict_proba(data, q_matrix)
+
+        # Assert y_pred_proba has correct data type
+        self.assertTrue(isinstance(y_pred_proba, np.ndarray))
+        for row in y_pred_proba:
+            self.assertTrue(isinstance(row, np.ndarray))
+            for element in row:
+                self.assertTrue(isinstance(element, float))
+
+        # Assert y_pred_proba is in correct shape
+        self.assertTrue(y_pred_proba.shape, (len(data), 2))
+
+    def test_predict(self):
+        """ Testing predict function """
+        # Data matrix
+        outcomes = [0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0]
+        data = []
+        for question_id, outcome in enumerate(outcomes):
+            data.append([outcome, 0, question_id])
+
+        # Q matrix
+        q_matrix = np.array([[1, 0]]*len(outcomes))
+        q_matrix[[0, 1], 1] = 1
+
+        # Instantiate model
+        model = BKT()
+        model.fit(data, q_matrix)
+
+        # Data for predicting should contain only one student
+        data = []
+        for question_id, outcome in enumerate(outcomes):
+            data.append([outcome, question_id])
+
+        y_pred = model.predict(data, q_matrix)
+
+        # Assert y_pred has correct data type
+        self.assertTrue(isinstance(y_pred, np.ndarray))
+        for outcome in y_pred:
+            self.assertTrue(isinstance(outcome, np.int64))
+
+        # Assert y_pred is in correct shape
+        self.assertTrue(y_pred.shape, (len(data),))
+
+    def test_score(self):
+        # Data matrix
+        outcomes = [0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0]
+        data = []
+        for question_id, outcome in enumerate(outcomes):
+            data.append([outcome, 0, question_id])
+
+        # Q matrix
+        q_matrix = np.array([[1, 0]]*len(outcomes))
+        q_matrix[[0, 1], 1] = 1
+
+        # Instantiate model
+        model = BKT()
+        model.fit(data, q_matrix)
+
+        # Data for predicting should contain only one student
+        data = []
+        for question_id, outcome in enumerate(outcomes):
+            data.append([outcome, question_id])
+
+        model.predict(data, q_matrix)
+        scores = model.score()
+
+        # Assert that all 4 scores are coming
+        self.assertTrue(len(scores), 4)
+
+        # Assert scores types
+        for value in scores:
+            self.assertTrue(isinstance(value, float))
+
+        # Assert scores values for current example
+        expected_scores = (36.685475118198525, 44.651333306630455,
+                           0.3009647032927156, 0.9)
+        for idx in range(len(scores)):
+            self.assertAlmostEqual(scores[idx], expected_scores[idx])
+
+
+if __name__ == '__main__':
+    unittest.main()
