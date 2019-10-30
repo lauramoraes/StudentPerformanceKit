49333e6ba6a40b1ba4496469b5f1db5c0b6d7428
Laura Moraes (Piano) <lmoraes@cos.ufrj.br>
2019-10-29 22:43:32 -0300

    Update PFA with learning state and bug-fixes

--- spkit/pfa.py
+++ spkit/pfa.py
@@ -36,6 +36,21 @@ class PFA(object):
         self.model_params = None
         # Params
         self.params = None
+        self.skills = None
+        
+        # Student learning state
+        self.learning_state = []
+        
+        # Evaluation metrics
+        self.n_skills = 0
+        self.n_questions = 0
+        self.outcomes = None
+        self.loglikelihood = 0
+        self.outcome_prob = 0
+        self.aic = 0
+        self.bic = 0
+        self.rmse = 0
+        self.acc = 0
 
     @staticmethod
     def _create_onehot(row, skills, skills_onehot):
@@ -44,6 +59,23 @@ class PFA(object):
         wins = row['wins']*skills_onehot[idx][0]
         fails = row['fails']*skills_onehot[idx][0]
         return np.concatenate((skills_onehot[idx][0], wins, fails))
+        
+    def _apply_onehot(self, data):
+        """ Transform data to its onehot format """
+        skills = self.skills
+        skills_onehot = self.params["skills_onehot"]
+        onehot_array = data.apply(self._create_onehot, axis=1,
+                                  args=(skills, 
+                                        skills_onehot))
+        cols = ["skills_%d"%skill for skill in skills]
+        cols += ["wins_%d"%skill for skill in skills]
+        cols += ["fails_%d"%skill for skill in skills]
+        onehot_df = pd.DataFrame(onehot_array.tolist(), columns=cols)
+        data = pd.concat((data, onehot_df), axis=1)
+        data = data.drop(columns=['skill', 'wins', 'fails'])
+        data = data.groupby(['index']).sum().astype({
+            'outcome': 'bool'}).astype({'outcome': 'int64'})
+        return data, cols
 
     def _skills_onehot(self, data):
         """ Transform PFA data to its onehot version where each columns
@@ -52,24 +84,59 @@ class PFA(object):
         skill_1 | skill_2 | wins_1 | wins_2 | fails_1 | fails_2
         """
         skills = data['skill'].unique()
+        self.skills = skills.tolist()
+        self.n_skills = len(self.skills)
         skills_array = skills.reshape(-1, 1)
         enc = OneHotEncoder(categories='auto', sparse=False)
         skills_onehot = enc.fit_transform(skills_array)
-        onehot_array = data.apply(self._create_onehot, axis=1,
-                                  args=(skills, skills_onehot))
-        cols = ["skills_%d"%skill for skill in skills]
-        cols += ["wins_%d"%skill for skill in skills]
-        cols += ["fails_%d"%skill for skill in skills]
-        onehot_df = pd.DataFrame(onehot_array.tolist(), columns=cols)
-        data = pd.concat((data, onehot_df), axis=1)
-        return data, cols
+        self.params["skills_onehot"] = skills_onehot
+        return skills_onehot
+		
+    def _transform_student_data(self, data, q_matrix, learning_state=None):
+        """ Transform original data into PFA expected format. Calculates wins,
+        fails, get skills and transform everything into one-hot variables """
+        student_skills_count = {}
+        pfa_data = []
+        for idx, row in enumerate(data):
+            outcome, question_id = row
+
+            # Get skill index in skills list
+            skills_idx = np.where(q_matrix[question_id, :] == 1)
+            # For each skill, calculate wins and fails
+            for skill in skills_idx[0]:
+                # Create skills for student if it's new
+                if skill not in student_skills_count:
+                    student_skills_count[skill] = defaultdict(int)
+
+                # Add row to PFA table
+                wins = student_skills_count[skill]["wins"]
+                fails = student_skills_count[skill]["fails"]
+                pfa_row = (idx, skill, wins, fails, outcome)
+                pfa_data.append(pfa_row)
+
+                # Update wins or fails counter
+                if outcome == 1:
+                    student_skills_count[skill]["wins"] += 1
+                else:
+                    student_skills_count[skill]["fails"] += 1
+
+        # Create dataframe from PFA data
+        df = pd.DataFrame(pfa_data, columns=["index", "skill", "wins",
+                                             "fails", "outcome"])
+        pfa_onehot = self._apply_onehot(df)
+        # Sum learning state (previous wins and fails)
+        if learning_state:
+            for idx, skill in enumerate(self.skills):
+                pfa_onehot["wins_%s" % skill] += learning_state[idx][0]
+                pfa_onehot["fails_%s" % skill] += learning_state[idx][1]
+        return pfa_onehot
 
     def _transform_data(self, data, q_matrix):
         """ Transform original data into PFA expected format. Calculates wins,
         fails, get skills and transform everything into one-hot variables """
         skills_count = {}
         pfa_data = []
-        for row in data:
+        for idx, row in enumerate(data):
             outcome, student_id, question_id = row
             # If student was not seen yet, create student counter
             if student_id not in skills_count:
@@ -86,7 +153,7 @@ class PFA(object):
                 # Add row to PFA table
                 wins = skills_count[student_id][skill]["wins"]
                 fails = skills_count[student_id][skill]["fails"]
-                pfa_row = (skill, wins, fails, outcome)
+                pfa_row = (idx, skill, wins, fails, outcome)
                 pfa_data.append(pfa_row)
 
                 # Update wins or fails counter
@@ -96,10 +163,10 @@ class PFA(object):
                     skills_count[student_id][skill]["fails"] += 1
 
         # Create dataframe from PFA data
-        df = pd.DataFrame(pfa_data, columns=["skill", "wins", "fails",
+        df = pd.DataFrame(pfa_data, columns=["index", "skill", "wins", "fails",
                                              "outcome"])
-
-        pfa_onehot = self._skills_onehot(df)
+        self._skills_onehot(df)
+        pfa_onehot = self._apply_onehot(df)
         return pfa_onehot
 
     def fit(self, data, q_matrix, **kwargs):
@@ -138,6 +205,7 @@ class PFA(object):
 
         """
         # Transform data to PFA format
+        self.params = {}
         data, cols = self._transform_data(data, q_matrix)
 
         # Fit model
@@ -152,7 +220,7 @@ class PFA(object):
         if self.lib == "sklearn":
             model = algorithm_lookup[self.lib](**params)
             model.fit(X, y)
-            self.params = np.concatenate((model.intercept_.reshape(-1,1),
+            self.params["weights"] = np.concatenate((model.intercept_.reshape(-1,1),
                                           model.coef_), axis=1)
         elif self.lib == "sm":
             # If sm library, it is needed to add the intercept
@@ -168,12 +236,13 @@ class PFA(object):
                 model = model.fit_regularized(**reg_params)
             else:
                 model = model.fit()
-            self.params = model.params.values.reshape(1,-1)
+            
+            self.params["weights"] = model.params.values.reshape(1,-1)
 
         self.model = model
         return self
 
-    def _predict(self, data, q_matrix):
+    def _predict(self, data, q_matrix, learning_state=None):
         """ Predict student outcomes based on trained model.
 
         Parameters
@@ -195,11 +264,13 @@ class PFA(object):
             outcome 0 (incorrect) and column 1 to outcome 1 (correct)
         """
         # Transform data to PFA format
-        data, cols = self._transform_data(data, q_matrix)
+        data, cols = self._transform_student_data(data, q_matrix, learning_state)
 
         # Fit model
         X = data[cols]
-        y = data['outcome']
+        self.outcomes = data['outcome']
+        self.n_questions = self.outcomes.shape[0]
+        
         if self.lib == "sklearn":
             self.outcome_prob = self.model.predict_proba(X)
         elif self.lib == "sm":
@@ -208,10 +279,20 @@ class PFA(object):
             self.outcome_prob = np.zeros((X.shape[0], 2))
             self.outcome_prob[:,1] = self.model.predict(X).values
             self.outcome_prob[:,0] = 1 - self.outcome_prob[:,1]
-
+            
+        # Update student learning state
+        for skill in self.skills:
+            worked_skill = X[X['skills_%s' % skill] == 1]
+            if not worked_skill.empty: 
+                wins = X[X['skills_%s' % skill] == 1]['wins_%s' % skill].tail(1).iloc[0]
+                fails = X[X['skills_%s' % skill] == 1]['fails_%s' % skill].tail(1).iloc[0]
+            else:
+                wins = 0
+                fails = 0
+    
         return self.outcome_prob
 
-    def predict_proba(self, data, q_matrix):
+    def predict_proba(self, data, q_matrix, learning_state=[]):
         """ Predict student outcome probabilities based on trained model.
 
         Parameters
@@ -233,10 +314,10 @@ class PFA(object):
             outcome 0 (incorrect) and column 1 to outcome 1 (correct)
 
         """
-        y_pred_proba = self._predict(data, q_matrix)
+        y_pred_proba = self._predict(data, q_matrix, learning_state)
         return y_pred_proba
 
-    def predict(self, data, q_matrix):
+    def predict(self, data, q_matrix, learning_state=[]):
         """ Predict student outcomes based on trained model. This is just the
         hard-assigment (highest probability) of the outcome probabilities.
 
@@ -258,23 +339,25 @@ class PFA(object):
             Outcome probabilites for steps in data. Column 0 corresponds to
             outcome 0 (incorrect) and column 1 to outcome 1 (correct)
         """
-        y_pred_proba = self._predict(data, q_matrix)
+        y_pred_proba = self._predict(data, q_matrix, learning_state)
         y_pred = np.argmax(y_pred_proba, axis=1)
         return y_pred
 
-    def score(self, y, y_pred_proba):
+    def score(self):
         """
         Calculates LL, AIC, BIC, RMSE and accuracy for the predicted sample
 
         """
+        y = self.outcomes
+        
         # LL
-        self.loglikelihood = (np.log(y_pred_proba[range(y.shape[0]),y]))
+        self.loglikelihood = (np.log(self.outcome_prob[range(y.shape[0]),y]))
 
         # AIC: 7 is the number of PFA parameters
-        self.aic = -2*self.loglikelihood.sum() + 2*7
+        self.aic = -2*self.loglikelihood.sum() + 2*3*self.n_skills
 
         # BIC: 7 is the number of PFA parameters
-        self.bic = -2*self.loglikelihood.sum() + 7*np.log(y.shape[0])
+        self.bic = -2*self.loglikelihood.sum() + 3*self.n_skills*np.log(y.shape[0])
 
         # RMSE
         rmse = ((1-self.outcome_prob[range(y.shape[0]), y])**2).sum()
@@ -307,3 +390,14 @@ class PFA(object):
         """
         self.params = params
         return self
+       
+    def get_learning_state(self):
+        """ Return last predict student learning state.
+
+        Returns
+        -------
+        learning_state: array. Array with last predicted student learning state value for each KC.
+        """
+        if self.learning_state is None:
+            raise RuntimeError("You should predict outcomes for a student before getting the learning state")
+        return self.learning_state
